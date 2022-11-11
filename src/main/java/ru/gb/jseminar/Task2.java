76280322d41134b4ad63906a6b0b51b2f4a01d64
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-11-11 12:31:19 +0300

    add Task0

--- src/main/java/ru/gb/jseminar/Task2.java
+++ src/main/java/ru/gb/jseminar/Task2.java
@@ -1,22 +1,27 @@
 package ru.gb.jseminar;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 public class Task2 {
 
-    // Вам дан массив путей, где пути[i] = [”Город А”, “Город Б”] означают, что существует прямой путь, идущий от
-    // ”Город А” до“Город Б”. Верните город назначения, то есть город без какого-либо пути, ведущего в другой город.
-    // Пример 1: Input: s = [["Москва","Самара"], ["Курск","Пенза"],["Самара","Курск"]]  Output: Пенза
-    // Пример 2: Input: s = [["Москва","Самара"]]  Output: Самара
-    public static void main(final String[] args) {
+	// Вам дан массив путей, где пути[i] = [”Город А”, “Город Б”] означают, что существует прямой путь, идущий от
+	// ”Город А” до“Город Б”. Верните город назначения, то есть город без какого-либо пути, ведущего в другой город.
+	// Пример 1: Input: s = [["Москва","Самара"], ["Курск","Пенза"],["Самара","Курск"]]  Output: Пенза
+	// Пример 2: Input: s = [["Москва","Самара"]]  Output: Самара
+	public static void main(final String[] args) {
+//		Task2 task2 = new Task2();
+//		List<Integer> taskList = new ArrayList<>(Arrays.asList(1, 2, 4, 33, 1, 100));
+//		System.out.println(task2.getFinalCity(taskList));
+	}
 
-    }
+	public String getFinalCity(final Map<String, String> map) {
+//		Map<Integer, Integer> checkMap = new HashMap();
 
-    public String getFinalCity(final Map<String, String> map){
 
-        return "";
-    }
+		return "";
+	}
 
 }

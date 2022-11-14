9d8d80d775bd86e4983c4e96b579679932d23eec
Evgeniy <evgen-z2007@yandex.ru>
2022-11-14 14:10:20 +0500

    "Домашнее задание к 5 семинару"

--- src/main/java/ru/gb/jseminar/Task0.java
+++ src/main/java/ru/gb/jseminar/Task0.java
@@ -1,18 +1,34 @@
 package ru.gb.jseminar;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class Task0 {
+public class  Task0 {
 
     // Дана последовательность чисел. Необходимо вернуть сумму уникальных чисел
+    //[1, 2, 3, 2]
     public static void main(final String[] args) {
+        Task0 t0 = new Task0();
+        List<Integer> taskList = new ArrayList<>(List.of(1, 2, 4, 33, 1, 100));
+        System.out.println(t0.getSumOfUniqueValues(taskList));
 
     }
 
-    public Integer getSumOfUniqueValues(final List<Integer> list){
+    public Integer getSumOfUniqueValues(final List<Integer> list) {
+        Map<Integer, Integer> checkMap = new HashMap<>();
+        for (Integer item : list) {
+            checkMap.put(item, 1 + checkMap.getOrDefault(item, 0));
 
-        return 0;
+        }
+        int result = 0;
+        for (Integer item: checkMap.values()){
+            if (item==1){
+                result++;
+            }
+        }
+        return result;
     }
 
 }
--- src/main/java/ru/gb/jseminar/Task1.java
+++ src/main/java/ru/gb/jseminar/Task1.java
@@ -1,7 +1,9 @@
 package ru.gb.jseminar;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Task1 {
 
@@ -9,13 +11,26 @@ public class Task1 {
     // Пример 1:     In: arr = [1, 2]  Out: 0
     // Пример 2:     In: arr = [1, 1, 2]  Out: 1
     // Пример 3:     In: arr = [1, 1, 1, 2, 2]  Out: 4
+    //(n/2)*(n-1) формула расчета кол-ва повторяющихся чисел
     public static void main(final String[] args) {
-
+        Task1 t1 = new Task1();
+        List<Integer> numberlist = new ArrayList<>(List.of(1, 1, 1, 2, 2));
+        System.out.println(t1.getNumberOfCouples(numberlist));
     }
 
     public Integer getNumberOfCouples(final List<Integer> list){
+        Map<Integer, Integer> numberlist = new HashMap<>();
+        for (Integer item : list) {
+            numberlist.put(item, 1 + numberlist.getOrDefault(item, 0));
+        }
+            int result = 0;
+        for (Integer item: numberlist.values()){
+            if(item>1){
+                result+= (double)item/2*(item-1);
+            }
 
-        return 0;
+        }
+        return result ;
     }
 
 }

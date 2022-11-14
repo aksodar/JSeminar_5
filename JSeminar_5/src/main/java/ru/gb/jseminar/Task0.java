b342618c6c3fc7ca99f4da03b6e6658192800d74
viktornazilin <104361827+viktornazilin@users.noreply.github.com>
2022-11-14 20:34:33 +0100

    Homework with unit tests is uploaded

---
+++ JSeminar_5/src/main/java/ru/gb/jseminar/Task0.java
@@ -0,0 +1,18 @@
+package ru.gb.jseminar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task0 {
+
+    // Дана последовательность чисел. Необходимо вернуть сумму уникальных чисел
+    public static void main(final String[] args) {
+
+    }
+
+    public Integer getSumOfUniqueValues(final List<Integer> list){
+
+        return 0;
+    }
+
+}

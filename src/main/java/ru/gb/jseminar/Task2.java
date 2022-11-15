fcf441f1e4f65617b4a2421d26fbc0ee34ef14f7
Inna Osipova <rutheva_inna@mail.ru>
2022-11-15 20:07:23 +0300

    Пункт назначения

--- src/main/java/ru/gb/jseminar/Task2.java
+++ src/main/java/ru/gb/jseminar/Task2.java
@@ -1,8 +1,10 @@
 package ru.gb.jseminar;
 
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.HashMap;
+
 import java.util.Map;
+
 
 public class Task2 {
 
@@ -11,12 +13,24 @@ public class Task2 {
     // Пример 1: Input: s = [["Москва","Самара"], ["Курск","Пенза"],["Самара","Курск"]]  Output: Пенза
     // Пример 2: Input: s = [["Москва","Самара"]]  Output: Самара
     public static void main(final String[] args) {
-
+        Map<String, String> route = new HashMap<>();
+        route.put("Москва","Самара");
+        //route.put("Курск","Пенза");
+        //route.put("Самара","Курск");
+       
+        Task2 task2 = new Task2();
+        System.out.println(task2.getFinalCity(route));
     }
 
     public String getFinalCity(final Map<String, String> map){
+        String city = "";
+        for (String item : map.values()) {
+            if(!item.equals(map.get(item))){
+                city = item;
+            }
+        }
 
-        return "";
+        return city;
     }
 
 }

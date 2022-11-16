fc3e10f80a88f252f358421f211b7e825dd88719
Vladimir_Artamonov <tomodachiva@gmail.com>
2022-11-16 23:56:19 +0300

    Homework was done

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,13 +1,55 @@
 package ru.gb.jseminar;
 
+import java.util.Arrays;
+//import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+//import java.util.TreeMap;
+//import java.util.logging.Logger;
+
 public class Homework {
 
-    // Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов.
-    // Пусть дан список сотрудников:Иван Иванов (и остальные, полный текст дз будет на платформе)
-    // Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений.
+    // Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1
+    // человек может иметь несколько телефонов.
+    // Пусть дан список сотрудников:Иван Иванов (и остальные, полный текст дз будет
+    // на платформе)
+    // Написать программу, которая найдет и выведет повторяющиеся имена с
+    // количеством повторений.
+    // Отсортировать по убыванию популярности.
+
+    // Пусть дан список сотрудников в формате строки
+    // "Ева,Дарахвелидзе,89010348765;Ева,Дарахвелидзе,89010344345;Андрей,Иванов,89050377654;Дмитрий,Пономаренко,89344322343;"
+    // Написать программу, которая найдет и выведет повторяющиеся имена с
+    // количеством повторений.
     // Отсортировать по убыванию популярности.
     public static void main(final String[] args) {
+        Homework hw = new Homework();
+        String text = "Ева,Дарахвелидзе,89010348765;Ева,Дарахвелидзе,89010344345;Андрей,Иванов,89050377654;Дмитрий,Пономаренко,89344322343;Андрей,Иванов,89150377654;Ева,Дарахвелидзе,89010344346;";
+        // Logger log = Logger.getLogger(Homework.class.getName());
+        // log.info("Result is:" + hw.creationOfContacts(text));
+        Map<String, Integer> result = hw.creationOfContacts(text);
 
+        result.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .forEach(System.out::println);
+
+    }
+
+    public Map<String, Integer> creationOfContacts(String text) {
+        Map<Long, List<String>> initialContactMap = new HashMap<Long, List<String>>();
+        Map<String, Integer> finalContactMap = new HashMap<String, Integer>();
+
+        String[] contacts = text.split(";");
+        for (String item : contacts) {
+            List<String> compilation = Arrays.asList(item.split(","));
+            initialContactMap.putIfAbsent(Long.parseLong(compilation.get(2)),
+                    Arrays.asList(compilation.get(0), compilation.get(1)));
+        }
+        for (List<String> item : initialContactMap.values()) {
+            finalContactMap.put(String.join(" ", item),
+                    1 + finalContactMap.getOrDefault(String.join(" ", item), 0));
+        }
+        return finalContactMap;
     }
 
 }
--- src/main/java/ru/gb/jseminar/Task0.java
+++ src/main/java/ru/gb/jseminar/Task0.java
@@ -1,18 +1,42 @@
 package ru.gb.jseminar;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
 
 public class Task0 {
 
-    // Дана последовательность чисел. Необходимо вернуть сумму уникальных чисел
+    // Дана последовательность чисел. Необходимо вернуть (сумму) количество
+    // уникальных чисел
+    // [1,2,3,2] -> 1,3 -> 2
     public static void main(final String[] args) {
+        Task0 task = new Task0();
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 2));
+        Logger log = Logger.getLogger(Task0.class.getName());
+        log.info(task.getSumOfUniqueValues(list).toString());
+        // System.out.println(task.getSumOfUniqueValues(list));
 
     }
 
-    public Integer getSumOfUniqueValues(final List<Integer> list){
+    public Integer getSumOfUniqueValues(final List<Integer> list) {
+        Map<Integer, Integer> newDict = new HashMap<Integer, Integer>();
+        for (int item : list) {
+            // newDict.putIfAbsent(item, count++);
+            newDict.put(item, 1 + newDict.getOrDefault(item, 0));
 
-        return 0;
+        }
+        int count = 0;
+        for (int value : newDict.values()) {
+            if (value == 1)
+                count++;
+        }
+
+        System.out.println(newDict);
+
+        return count;
     }
 
 }

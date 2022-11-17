3df0a3770d25d887ecc9744eef1e6dcee9ecf42e
Maria <ma.pechenkina@yandex.ru>
2022-11-17 22:09:44 +0300

    Промежуточное решение домашней работы. Need help

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,5 +1,7 @@
 package ru.gb.jseminar;
 
+import java.util.*;
+
 public class Homework {
 
     // Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов.
@@ -7,7 +9,34 @@ public class Homework {
     // Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений.
     // Отсортировать по убыванию популярности.
     public static void main(final String[] args) {
+        Homework h = new Homework();
 
+        String inputString = "Ева,Дарахвелидзе,89010348765;Ева,Дарахвелидзе,89010344345;Андрей,Иванов,89050377654;Дмитрий,Пономаренко,89344322343";
+        System.out.println(h.getSortedIncludesWords(inputString));
     }
-
+    public String getSortedIncludesWords(final String inputString){
+        List<String> inputStringListFirst = Arrays.asList(inputString.toLowerCase().split(";"));
+        List<String> inputStringListSecond = Arrays.asList(inputStringListFirst.toString().split(","));
+        List<String> listNames = new ArrayList<>();
+        List<String> listNumbers = new ArrayList<>();
+        String temp = "";
+        for (int i = 0; i < inputStringListSecond.size();i = i + 3){
+            temp = inputStringListSecond.get(i) + " " + inputStringListSecond.get(i+1);
+            listNames.add(temp);
+            listNumbers.add(inputStringListSecond.get(i+2));
+        }
+        Map<String, List<String>> ResultMap = new TreeMap<>();
+        for (int i = 0; i < listNames.size(); i++){
+            if (ResultMap.containsKey(listNames.get(i))){
+                List<String> temp2 = ResultMap.get(listNames.get(i));
+                temp2.add(listNumbers.get(i));
+                System.out.println(temp2);
+                ResultMap.put(listNames.get(i),temp2);
+            } else {
+                List<String> temp2 = Arrays.asList(listNumbers.get(i));
+                ResultMap.put(listNames.get(i),temp2);
+            }
+        }
+        return ResultMap.toString();
+    }
 }

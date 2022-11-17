06b0a0be4fa29c55118eb759a60cf703291f7b9f
Vladimir_B <vladimir.baida@gmail.com>
2022-11-17 22:34:02 +0300

    ДЗ по 5-му семинару

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,13 +1,40 @@
 package ru.gb.jseminar;
 
+import java.util.*;
+import java.util.logging.Logger;
+
 public class Homework {
 
     // Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов.
     // Пусть дан список сотрудников:Иван Иванов (и остальные, полный текст дз будет на платформе)
     // Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений.
     // Отсортировать по убыванию популярности.
-    public static void main(final String[] args) {
 
+    //"Ева,Дарахвелидзе,89010348765;Ева,Дарахвелидзе,89010344345;Андрей,Иванов,89050377654;Дмитрий,Пономаренко,89344322343;"
+    public static void main(final String[] args) {
+        String str = "Ева,Дарахвелидзе,89010348765;Андрей,Иванов,89050377654;Андрей,Иванов,89050377654;Ева,Дарахвелидзе,89010344345;Ева,Дарахвелидзе,89010348765;Дмитрий,Пономаренко,89344322343;";
+        Homework hw = new Homework();
+        Logger log = Logger.getLogger(Homework.class.getName());
+        log.info(String.valueOf(hw.getSortedPopular(str)));
     }
 
+    public List<String> getSortedPopular(final String str){
+        List<String> strList = Arrays.asList(str.toLowerCase().split(";"));
+        Map<String, Integer> strMap = new HashMap<>();
+        for (int i = 0; i < strList.size(); i++) {
+            List<String> rowList = Arrays.asList(strList.get(i).split(","));
+            String nameSurname = rowList.get(0) + " " + rowList.get(1);
+            if (strMap.containsKey(nameSurname)) {
+                strMap.put(nameSurname, 1 + strMap.getOrDefault(nameSurname, 0));
+            } else {
+                strMap.put(nameSurname, 1);
+            }
+        }
+        List <String> lst = new ArrayList<>();
+        for (Map.Entry<String, Integer> entrX : strMap.entrySet()) {
+             lst.add(0, (entrX.getValue()) + " " + (entrX.getKey()));
+        }
+        Collections.sort(lst, Comparator.reverseOrder());
+        return lst;
+    }
 }
--- src/main/java/ru/gb/jseminar/Task3.java
+++ src/main/java/ru/gb/jseminar/Task3.java
@@ -1,18 +1,41 @@
 package ru.gb.jseminar;
 
-import java.util.Map;
+import java.util.*;
+import java.util.logging.Logger;
 
 public class Task3 {
 
     // Дана строка. Необходимо написать метод, который отсортирует слова в строке по длине с помощью TreeMap.
     // Строки с одинаковой длиной не должны “потеряться”.
+    // мороз и солонце день чудесный еще ты дремлешь друг прелестный
     public static void main(final String[] args) {
-
+        String str = "мороз и солнце день чудесный еще ты дремлешь друг прелестный";
+        Task3 tsk = new Task3();
+        Logger log = Logger.getLogger(Task3.class.getName());
+        log.info(String.valueOf(tsk.getSortedIncludesWords(str)));
     }
 
-    public String getSortedIncludesWords(final Map<String, String> map){
+    public String getSortedIncludesWords(final String str){
+        List<String> strList = Arrays.asList(str.toLowerCase().split(" "));
+        Map<Integer, List<String>> strMap = new TreeMap<>();
+        for (String item: strList) {
+           if (strMap.containsKey(item.length())){
+               List<String> tempLst = strMap.get(item.length());
+               tempLst.add(item);
+               strMap.put(item.length(),tempLst);
+           } else {
+               List<String> tempLst = new ArrayList<>(List.of(item));
+               strMap.put(item.length(),tempLst);
+           }
 
-        return "";
+        }
+        System.out.println(strMap);
+        StringBuilder strBd = new StringBuilder();
+        for (List <String> elem: strMap.values()){
+            strBd.append(elem);
+        }
+
+        return String.valueOf(strBd);
     }
 
 }

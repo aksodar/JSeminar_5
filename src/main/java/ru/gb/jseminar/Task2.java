efbe4b9f78be9530aa7ccd12b0913a2713e128f1
YuliyaStuchilova <yulia.s9314@gmail.com>
2022-11-26 01:23:50 +0300

    Homework

--- src/main/java/ru/gb/jseminar/Task2.java
+++ src/main/java/ru/gb/jseminar/Task2.java
@@ -1,7 +1,9 @@
 package ru.gb.jseminar;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class Task2 {
 
@@ -10,11 +12,26 @@ public class Task2 {
     // Пример 1: Input: s = [["Москва","Самара"], ["Курск","Пенза"],["Самара","Курск"]]  Output: Пенза
     // Пример 2: Input: s = [["Москва","Самара"]]  Output: Самара
     public static void main(final String[] args) {
+        Task2 tk2 = new Task2();
+        Logger lg = Logger.getLogger(Task2.class.getName());
+        Map<String, String> map = new HashMap<>();
+        map.putIfAbsent("Москва","Самара");
+        map.putIfAbsent("Курск","Пенза");
+        map.putIfAbsent("Самара","Курск");
+        System.out.println(map);
+        lg.info(String.valueOf(tk2.getFinalCity(map)));
 
     }
 
     public String getFinalCity(final Map<String, String> map){
-        return "";
+
+        for (String item:map.values()) {
+            if(!map.containsKey(item)){
+                return item;
+            }
+
+        }
+        throw new IllegalStateException();
     }
 
 }
--- src/main/java/ru/gb/jseminar/Task3.java
+++ src/main/java/ru/gb/jseminar/Task3.java
@@ -1,18 +1,41 @@
 package ru.gb.jseminar;
 
-import java.util.Map;
+import java.util.*;
+import java.util.logging.Logger;
 
 public class Task3 {
 
     // Дана строка. Необходимо написать метод, который отсортирует слова в строке по длине с помощью TreeMap.
     // Строки с одинаковой длиной не должны “потеряться”.
+    // Мороз и солнце день чудесный Ещё ты дремлешь друг прелестный
     public static void main(final String[] args) {
+        Task3 tk3 = new Task3();
+        Logger lg = Logger.getLogger(Task3.class.getName());
 
+        String text = "Мороз и солнце день чудесный Ещё ты дремлешь друг прелестный";
+
+        lg.info(String.valueOf(tk3.getSortedIncludesWords(text)));
     }
 
-    public String getSortedIncludesWords(final Map<String, String> map){
+    public String getSortedIncludesWords(final String text){
+        List<String> textList = Arrays.asList(text.toLowerCase().split(" "));
+        Map<Integer,List<String>> map = new TreeMap<>();
+        for (String item:textList){
+            if(map.containsKey(item.length())){
+                List<String> temp = map.get(item.length());
+                temp.add(item);
+                map.put(item.length(),temp);
+            }else {
+                List<String> temp = new ArrayList<>(List.of(item));
+                map.put(item.length(),temp);
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (List<String> item: map.values()) {
+            result.append(item);
 
-        return "";
+        }
+        return String.valueOf(result);
     }
 
 }

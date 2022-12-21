9f5cd1d7c37e9409be0984ee8b11576754222a4f
Lera <valeriiakolesnikova21@gmail.com>
2022-12-21 15:46:21 +0300

    homework 5 added

--- src/main/java/ru/gb/jseminar/Task1.java
+++ src/main/java/ru/gb/jseminar/Task1.java
@@ -1,6 +1,6 @@
 package ru.gb.jseminar;
 
-import java.util.List;
+import java.util.*;
 
 public class Task1 {
 
@@ -9,12 +9,27 @@ public class Task1 {
     // Пример 2:     In: arr = [1, 1, 2]  Out: 1
     // Пример 3:     In: arr = [1, 1, 1, 2, 2]  Out: 4
     public static void main(final String[] args) {
-
+        List <Integer> array = new ArrayList<>(Arrays.asList(1, 2));
+        Task1 c = new Task1();
+        System.out.println(c.getNumberOfCouples(array));
     }
 
     public Integer getNumberOfCouples(final List<Integer> list){
+        Map<Integer, Integer> book = new HashMap<>();
 
-        return 0;
+        for (Integer i : list){
+            if(!book.containsKey(i)){
+                book.put(i, 1);
+            } else {
+                book.put(i, book.get(i) + 1);
+            }
+        }
+        int sum = 0;
+        for(Integer i : book.keySet()){
+            if(book.get(i) != 1) {
+                sum += (book.get(i) * (book.get(i) - 1))/2;
+            }
+        }
+        return sum;
     }
-
 }

480ba6c186aabd3ebf927e553436abe43a077929
Rendo-M <srvr@list.ru>
2022-12-21 19:29:45 +0300

    homework was added

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,13 +1,61 @@
 package ru.gb.jseminar;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Homework {
 
     // Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов.
     // Пусть дан список сотрудников:Иван Иванов (и остальные, полный текст дз будет на платформе)
-    // Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений.
-    // Отсортировать по убыванию популярности.
+    // Вывести список пользователей отсортированный по количествоу телефонов.
     public static void main(final String[] args) {
+        Homework hw = new Homework();
+        Map<String, List<String>> phonebook = new HashMap<>(); 
+        phonebook = hw.addPhone("Иванов", "88005553535", phonebook);
+        phonebook = hw.addPhone("Иванов", "88005653535", phonebook);
+        phonebook = hw.addPhone("Иванов", "88005753535", phonebook);
+        phonebook = hw.addPhone("Петров", "777-555", phonebook);
+        phonebook = hw.addPhone("Петров", "555-777", phonebook);
+        phonebook = hw.addPhone("Андреев", "112233", phonebook);
+        phonebook = hw.addPhone("Андреев", "332211", phonebook);
+        phonebook = hw.addPhone("Саня", "11211", phonebook);
+        phonebook = hw.addPhone("Толик", "22112", phonebook);        
+        System.out.println(phonebook);
+        System.out.println(hw.sortPhonebook(phonebook));
+    }
+    
+    
+    public Map<String, List<String>> addPhone(String name, String phone, Map<String, List<String>> phonebook){
+        if (phonebook.containsKey(name)){
+            List<String> temp = phonebook.get(name);
+            temp.add(phone);
+            phonebook.put(name, temp);  
+        }
+        else{
+            List<String> temp = new ArrayList<>();
+            temp.add(phone);
+            phonebook.put(name, temp);
+        }
+        return phonebook;
+    }; 
 
+    public Map<Integer, Map<String, List<String>>> sortPhonebook(Map<String, List<String>> phonebook){
+        Map<Integer, Map<String, List<String>>> sortedPhones = new HashMap<>();
+        for (String name : phonebook.keySet()) {
+            if (!sortedPhones.containsKey(phonebook.get(name).size())){
+                Map<String, List<String>> temp = new HashMap<>();
+                temp.put(name, phonebook.get(name));
+                sortedPhones.put(phonebook.get(name).size(), temp);
+            }
+            else{
+                Map<String, List<String>> temp = sortedPhones.get(phonebook.get(name).size());
+                temp.put(name, phonebook.get(name));
+                sortedPhones.put(phonebook.get(name).size(), temp);  
+            }
+        }    
+        return sortedPhones;
     }
 
 }
--- src/main/java/ru/gb/jseminar/Task0.java
+++ src/main/java/ru/gb/jseminar/Task0.java
@@ -1,17 +1,31 @@
 package ru.gb.jseminar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Task0 {
 
     // Дана последовательность чисел. Необходимо вернуть сумму уникальных чисел
     public static void main(final String[] args) {
-
+        Task0 task0 = new Task0();    
+        System.out.println(task0.getSumOfUniqueValues(new ArrayList<>(Arrays.asList(1,2,3,4,4))));
     }
 
     public Integer getSumOfUniqueValues(final List<Integer> list){
-
-        return 0;
+        Map<Integer, Integer> unique = new HashMap<>();
+        int result = 0;
+        for (Integer i : list) {
+            int count = unique.getOrDefault(i,0)+1;
+            unique.put(i, count);
+            if (count == 1) 
+                result = result + i;
+            else
+            if (count == 2) 
+                result = result - i;
+        }
+        return result;
     }
-
 }

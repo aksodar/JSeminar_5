707aa8fecc383fb3b47e54492f4d13b3d3976c74
Serg-Geek <svgordienko@yandex.ru>
2023-01-08 20:33:20 +0400

    add homework seminar5

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,13 +1,72 @@
 package ru.gb.jseminar;
 
+import java.util.*;
+
 public class Homework {
 
     // Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов.
-    // Пусть дан список сотрудников:Иван Иванов (и остальные, полный текст дз будет на платформе)
-    // Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений.
-    // Отсортировать по убыванию популярности.
+// Написать программу, которая выведет список людей с их телефонами
+// Отсортировать по колличеству телефонов.
     public static void main(final String[] args) {
+        Homework hw = new Homework();
+        String name = " Mask Ilon  +7-888-555-33-22, Ivanov Ivan Ivanovich 8-915-555-66-44, mask Ilon  8-917-564-89-96, Sidorov Sidor 69-69-69, " +
+                "Gates Bill 55-96-55, Gates Bill 78-89-55, Gates Bill +7918+555-66-77, petrov petr 56-88-96 ";
+        System.out.println(hw.telBook(name));
+        System.out.println(hw.sortphonebok(hw.telBook(name )));
+        hw.printtelbook(hw.telBook(name));
 
     }
 
+    public Map<String, List<String>> telBook(String text) {
+        Map<String, List<String>> book = new HashMap<>();
+        List<String> list = Arrays.asList(text.split(","));
+        for (String i : list) {
+            List<String> temp = Arrays.asList(i.trim().toUpperCase().split(" "));
+            String nam = null;
+            if (temp.size() == 4) {
+                nam = temp.get(0) + " " + temp.get(1) + " " + temp.get(2);
+            } else {
+                nam = temp.get(0) + " " + temp.get(1);
+            }
+            if (!book.containsKey(nam)) {
+                List<String> telbook = new ArrayList<>();
+                telbook.add(temp.get(temp.size() - 1));
+                book.put(nam, telbook);
+            } else {
+                String phone = temp.get(temp.size() - 1);
+                book.get(nam).addAll(Arrays.asList(phone));
+            }
+        }
+        return book;
+    }
+
+    public  Map<String,List<String>> printtelbook(Map<String,List<String>> book){
+        for (String item: book.keySet()) {
+            System.out.println(item + book.get(item));
+        }
+        return book;
+    }
+
+    public Map<Integer, List<String>> sortphonebok(Map<String, List<String>> book) {
+        Map<Integer, List<String>> result = new HashMap<>();
+        {
+
+            for (String item : book.keySet()) {
+                if (book.get(item).size() > 0) {
+                    int count = book.get(item).size();
+                    if (!result.containsKey(count)) {
+                        List<String> name = new ArrayList<>();
+                        name.add(item+book.get(item));
+                        result.put(count, name);
+                    } else {
+                        String name = item + book.get(item);
+                        result.get(count).addAll(Arrays.asList(name));
+                    }
+                }
+            }
+            return result;
+        }
+    }
 }
+
+
--- src/main/java/ru/gb/jseminar/Task0.java
+++ src/main/java/ru/gb/jseminar/Task0.java
@@ -1,17 +1,33 @@
 package ru.gb.jseminar;
 
-import java.util.List;
+import java.util.*;
+
+import static java.util.Arrays.*;
 
 public class Task0 {
 
     // Дана последовательность чисел. Необходимо вернуть сумму уникальных чисел
     public static void main(final String[] args) {
-
+        List<Integer> array = new ArrayList<>(asList(1, 2, 4, 5, 2, 9, 7));
+        Task0 db = new Task0();
+        System.out.println("sum " + db.getSumOfUniqueValues(array));
     }
 
-    public Integer getSumOfUniqueValues(final List<Integer> list){
-
-        return 0;
+    public Integer getSumOfUniqueValues(final List<Integer> list) {
+        Map<Integer, Integer> dct = new HashMap<>();
+        for (Integer i : list) {
+            if (!dct.containsKey(i)) {
+                dct.put(i, 0);
+            } else {
+                dct.put(i, dct.get(i) + 1);
+            }
+        }
+        int sum = 0;
+        for (Integer j : dct.keySet()) {
+            if (dct.get(j) == 0) {
+                sum += j;
+            }
+        }
+        return sum;
     }
-
 }

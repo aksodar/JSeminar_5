bb37ce5e62a9dd31bfad72991903f9244d11bfe2
Aleksey <leha_nizhnikov@mail.ru>
2023-01-12 18:46:14 +0500

    "Add home work 5"

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,13 +1,48 @@
 package ru.gb.jseminar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Homework {
 
     // Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов.
-    // Пусть дан список сотрудников:Иван Иванов (и остальные, полный текст дз будет на платформе)
-    // Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений.
-    // Отсортировать по убыванию популярности.
+    // Написать программу, которая выведет список людей с их телефонами
+    // Отсортировать по колличеству телефонов.
+
     public static void main(final String[] args) {
 
+        List <String> users = new ArrayList<String>( Arrays.asList("Александров Н.В. +795000333 +7951666333 +7951777333", 
+                                                                        "Нижников А.С. +7951111222 +7951222333", 
+                                                                        "Кант И. +7951444555 +7951333444 +7951222888", 
+                                                                        "Путин В.В. 911 112", 
+                                                                        "Толстой Л.Н. +7951555666", 
+                                                                        "Пушкин А.С. +795188858"));
+
+        Homework hm = new Homework();
+        System.out.println(hm.faindDoubleUser(users));
     }
 
+    public Map<Integer, List<String>> faindDoubleUser(List<String> list){
+        Map <Integer, List<String>> user_book = new HashMap<>();
+
+        for(String user: list){
+            String [] info_user = user.split(" ");
+            Integer quantity_numb = info_user.length - 2;
+            List<String> users = new ArrayList<>();
+
+            if(!user_book.containsKey(quantity_numb)){
+                users.add(user);
+            }
+            else{
+                users = user_book.get(quantity_numb);
+                users.add(user);
+            }
+            user_book.put(quantity_numb, users);
+        }
+
+        return user_book;
+    }
 }
